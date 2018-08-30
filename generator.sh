76ff07be6723b39efaafd7110f294b257d71aa84
jiyoungAn <cacoog@gmail.com>
2018-08-29 18:00:55 -0700

    Made script for only generator, Add vivado file

--- generator.sh
+++ generator.sh
@@ -10,9 +10,9 @@ Data_type="ap_uint<16>"
 Random_range="20"
 return_dir="../"
 
-cd keras-verification
-./verifier.sh $return_dir$Test_dir $Random_range
+#cd keras-verification
+#./verifier.sh $return_dir$Test_dir $Random_range
 
-cd ../c-code-generation
+cd c-code-generation
 python Test_cpp_Generator.py $return_dir$Test_dir $Model_name $Data_type
 python Cpp_Generator.py $return_dir$Test_dir $Model_name $Data_type
