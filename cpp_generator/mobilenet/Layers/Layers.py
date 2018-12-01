2346edd93f88bb00c50fce027ae2f1f79bfa7aa9
kangsj123 <kangsj123@naver.com>
2018-11-30 17:30:42 -0800

    code_edit

--- cpp_generator/mobilenet/Layers/Layers.py
+++ cpp_generator/mobilenet/Layers/Layers.py
@@ -137,12 +137,20 @@ class Activation(Layers):
         # init part
 
         # code
-        rl = open("cpp_generator/mobilenet/Template/Function/Softmax.txt")
-        template = rl.read()
-        func = template.format(Name=self.config["name"], Input_channel=input_shape[3], Input_width=input_shape[1],
-                               Input_height=input_shape[2], Output_channel=output_shape[3],
-                               Output_width=output_shape[1], Output_height=output_shape[2])
-        self.function['code'] = func + "\n"
+        if self.config['activation'] == 'relu':
+            rl = open("cpp_generator/inceptionv3/Template/Function/Activation_relu.txt")
+            template = rl.read()
+            func = template.format(Name=self.config["name"], Input_channel= input_shape[3],
+                                   Input_width= input_shape[1], Input_height=input_shape[2],
+                                   Output_channel=output_shape[3], Output_width=output_shape[1],
+                                   Output_height=output_shape[2])
+            self.function['code'] = func + "\n"
+        elif self.config['activation'] == 'softmax':
+            rl = open("cpp_generator/inceptionv3/Template/Function/Activation_softmax.txt")
+            template = rl.read()
+            func = template.format(Name=self.config["name"], Output_channel=output_shape[3],
+                                   Input_channel=input_shape[3])
+            self.function['code'] = func + "\n"
 
 
 class Reshape(Layers):
